566a6ac60d08249bc33cc1d46c4087657345ed01
David Hakobjan <d.hakobjan@chi-deutschland.com>
2024-05-02 00:08:50 +0200

    Added bullets to the game

--- main.py
+++ main.py
@@ -15,35 +15,73 @@ pygame.display.set_caption("Diep.io 2.0")
 PLAYER_WIDTH, PLAYER_HEIGHT = 40, 40
 player = pygame.rect.Rect(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_WIDTH, PLAYER_HEIGHT)
 
+# Bullet
+bullets = []
+
 # Clock to set and track FPS
 clock = pygame.time.Clock()
 FPS = 60
 
 run = True
 
-while run:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-            
-    keys = pygame.key.get_pressed()
+def shooting(player, keys):
+    if keys[pygame.K_UP]:
+        if len(bullets) < 5:
+            bullets.append({
+                "position": [player.centerx, player.top],
+                "original_y": player.top,
+                "radius": 10,
+                "direction": "up", 
+                "bullet_velocity": 10,
+                "bullet_range": 200})
 
-    if keys[pygame.K_a]:
-        player.x -= 10
-    if keys[pygame.K_d]:
-        player.x += 10 
-    if keys[pygame.K_w]:
-        player.y -= 10
-    if keys[pygame.K_s]:
-        player.y += 10   
+def update_bullets():
+    for bullet in bullets:
+        if bullet["direction"] == "up":
+            # Calculate the new position
+            new_y = bullet["position"][1] - bullet["bullet_velocity"]
 
-    screen.fill(BLACK)     
+            if abs(new_y - bullet["original_y"]) > bullet["bullet_range"]:
+                bullets.remove(bullet)
+            else:
+                bullet["position"][1] = new_y
 
-    pygame.draw.rect(screen, WHITE, player)
-    
-    pygame.display.flip()
+def draw_bullets():
+    for bullet in bullets:
+        pygame.draw.circle(screen, WHITE, bullet["position"], bullet["radius"])
 
-    clock.tick(FPS)
+def main():
+    global run
 
-pygame.quit()
-    
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            player.x -= 10
+        if keys[pygame.K_d]:
+            player.x += 10 
+        if keys[pygame.K_w]:
+            player.y -= 10
+        if keys[pygame.K_s]:
+            player.y += 10   
+
+        screen.fill(BLACK)     
+
+        pygame.draw.rect(screen, WHITE, player)
+        shooting(player, keys)
+        update_bullets()
+        draw_bullets()
+
+        pygame.display.flip()
+
+        clock.tick(FPS)
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
