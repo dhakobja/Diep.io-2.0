33823f5350707e2871452c97b9e6d7cdc0e669d5
dhakobja <dhakobja@web.de>
2024-05-02 22:51:33 +0200

    Added a xp bar

--- Players/player.py
+++ Players/player.py
@@ -6,6 +6,7 @@ class Player:
         self.name = name
         self.level = level
         self.xp = 0
+        self.max_xp = 100
         self.width = 40
         self.height = 40
         self.speed = 5
@@ -44,6 +45,15 @@ class Player:
         for bullet in self.bullets:
             bullet.draw(screen, camera.apply(bullet.position))
 
+    def add_xp(self, xp_value):
+        self.xp += xp_value
+        if self.xp >= 100:
+            self.update_level()
+    
+    def update_level(self):
+        self.level += self.xp // 100
+        self.xp = 0
+
     def draw(self, screen, camera):
         # Draw the player, but apply the camera offset first
         adjusted_position = camera.apply(self.position)
@@ -53,10 +63,16 @@ class Player:
         font = pygame.font.Font(None, 24)
         level_text = font.render(f"Lvl: {self.level}",True, (255, 255, 255))
         screen.blit(level_text, (adjusted_position[0], adjusted_position[1] - 20))
+
+        # Draw the xp Bar    
+        xp_bar_length = screen.get_width()
+        xp_bar_height = 5
+        xp_bar_x = self.position[0] + 100
+        xp_bar_y = self.position[1] + 100
+
+        # Background of the xp bar (red)
+        pygame.draw.rect(screen, (255, 0, 0), (adjusted_position[0] - screen.get_width() // 2, adjusted_position[1] + screen.get_height() // 2 - 10, xp_bar_length, xp_bar_height))
     
-    def add_xp(self, xp_value):
-        self.xp += xp_value
-        self.update_level()
-    
-    def update_level(self):
-        self.level = self.xp // 100 + 1
+        # Current xp (green)
+        current_xp_length = (self.xp / self.max_xp) * xp_bar_length
+        pygame.draw.rect(screen, (0, 255, 0), (adjusted_position[0] - screen.get_width() // 2, adjusted_position[1] + screen.get_height() // 2 - 10, current_xp_length, xp_bar_height))
